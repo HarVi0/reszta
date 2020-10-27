bc1764220dd1cff697d5af65a6eb5ba4ddd60899
HarVi0 <55979894+HarVi0@users.noreply.github.com>
2020-10-27 21:36:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,28 @@ using namespace std;
 
 int main()
 {
-    float nominal[9]= {500, 200, 100, 50, 20, 10, 5, 2, 1};
-    int reszta, liczba_banknotow, i=0;
+    float nominal[15]= {50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+    float  reszta;
+    int liczba_banknotow, i=0;
 
     cout << "Podaj reszte: ";
     cin >> reszta;
+    reszta = reszta * 100;
 
-    while (reszta > 0)
+    while (reszta>0)
     {
         if (reszta >= nominal[i])
         {
-            liczba_banknotow = reszta / nominal[i];
-            reszta = reszta - (nominal[i] * liczba_banknotow);
-            if(liczba_banknotow == 1)
+            liczba_banknotow=reszta / nominal[i];
+            reszta=reszta-(nominal[i]*liczba_banknotow);
+            if(liczba_banknotow==1)
             {
-                cout << liczba_banknotow << " raz " << nominal[i] <<" zl." << endl;
+
+                cout << liczba_banknotow << " raz " << nominal[i]/100 <<" zl." << endl;
             }
             else
             {
-                cout << liczba_banknotow << " razy " << nominal[i] <<" zl. " << endl;
+                cout << liczba_banknotow << " razy " << nominal[i]/100 <<" zl. " << endl;
             }
         }
         i++;
@@ -30,3 +33,4 @@ int main()
 
     return 0;
 }
+
